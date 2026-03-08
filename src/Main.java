2d63274bf51cf148308667b73e89bd15c1c3d144
NeiviRodriguez <neivi.rodriguez1@gmail.com>
2026-03-08 12:56:32 -0500

    ej herencia

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@
 
 void main() {
     //herencia
-            // Crear el objeto de la clase hija
-            Moto miMoto = new Moto();
+    // Crear el objeto de la clase hija
+    Moto miMoto = new Moto();
 
-            // traer datos con getter
-            System.out.println("La marca del vehiculo es: " + miMoto.getMarca());
-            System.out.println("El cilindraje es: " + miMoto.getCilindraje());
+    // traer datos con getter
+    System.out.println("La marca del vehiculo es: " + miMoto.getMarca());
+    System.out.println("El cilindraje es: " + miMoto.getCilindraje());
 
 
 }
